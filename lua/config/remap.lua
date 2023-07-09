92f8c38f1a2ac4635815e404c45ef8f63efd34e5
Renell Castro <renellc9819@gmail.com>
2023-07-09 01:23:56 -0700

    Updated remaps and lazy-lock.json

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -5,5 +5,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "<F1>", "<cmd>Lspsaga code_action<CR>", opts)
         vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+        vim.keymap.set("n", "<leader>jdf", vim.lsp.buf.definition, opts);
+        vim.keymap.set("n", "<leader>jdc", vim.lsp.buf.declaration, opts);
+
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, opts);
     end
 })
